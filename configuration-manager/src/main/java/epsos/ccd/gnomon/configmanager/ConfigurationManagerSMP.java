3009c323e9ee90e44ac41172994ba8b63a6d86b5
subiger <jerome.subiger@ext.ec.europa.eu>
2016-10-11 16:20:05 +0200

    Update of the pom.xml files, removing epSOS references. Update artifactId names openncp-XXX. Partially solve JIRA [OPENNCP-1071].

--- configuration-manager/src/main/java/epsos/ccd/gnomon/configmanager/ConfigurationManagerSMP.java
+++ configuration-manager/src/main/java/epsos/ccd/gnomon/configmanager/ConfigurationManagerSMP.java
@@ -12,11 +12,11 @@ import eu.epsos.configmanager.database.model.Property;
 
 /**
  * ConfigurationManagerSMP. Obtain a configuration value. Usage: <br/>
- * 
+ *
  * <pre>
  * ConfigurationManagerSMP.getInstance().getProperty(sampleKey);
  * </pre>
- * 
+ *
  * This class uses <a href=
  * "http://docs.oasis-open.org/bdxr/bdx-smp/v1.0/cs01/bdx-smp-v1.0-cs01.html">BDXR-SMP</a>
  * and <a href=
@@ -41,7 +41,7 @@ import eu.epsos.configmanager.database.model.Property;
  * SSLPeerUnverified), the SMP query is performed</li>
  * </ol>
  * One point of intervention would be to add a TTL to a value in the hashmap.
- * 
+ *
  * @author massimiliano.masi@bmg.gv.at
  *
  */
@@ -64,7 +64,7 @@ public final class ConfigurationManagerSMP {
 
     /**
      * Get an instance of the ConfigurationManagerSMP.
-     * 
+     *
      * @return an instance of the ConfigurationManagerSMP class.
      */
     public static ConfigurationManagerSMP getInstance() {
@@ -83,20 +83,20 @@ public final class ConfigurationManagerSMP {
      * Constructor. Here I have to load all the properties from the database.
      */
     private ConfigurationManagerSMP() {
-        
+
         long start = System.currentTimeMillis();
         l.debug("Loading the Hibernate session object");
         session = HibernateUtil.getSessionFactory().openSession();
         long end = System.currentTimeMillis();
         long total = end-start;
         l.trace("Loaded took: " + total);
-        
-        
+
+
         populate();
         l.debug("Constructor ends");
     }
 
-    
+
     /**
      * Adds the values from the DB to the memory
      */
@@ -110,7 +110,7 @@ public final class ConfigurationManagerSMP {
         int size = properties.size();
         l.debug("Adding " + size + " properties into the hashmap");
 
-        
+
         start = System.currentTimeMillis();
         // Not using streams because of the mandatory requirement to use java7
         for (int i=0; i<size; i++) {
@@ -122,58 +122,59 @@ public final class ConfigurationManagerSMP {
         }
         end = System.currentTimeMillis();
         total = end-start;
-        l.trace("Loading in memory the full database took: " + total);        
+        l.trace("Loading in memory the full database took: " + total);
     }
 
+    //TODO: ckeck anf fix missing classes and cycling maven dependency to tsl-utils component
     /**
      * Obtain a property configuration property.
-     * @param key The key to search. 
+     * @param key The key to search.
      * @return The given property, if found
      * @throws PropertyNotFoundException if the property can't be found either in the hashmap, SMP, or after TSLSynchronizer
      */
-    public String getProperty(String key) throws PropertyNotFoundException {
+    public String getProperty(String key) /*throws PropertyNotFoundException*/ {
         l.debug("Searching for " + key);
         l.debug("Trying hashmap first");
         String value = configuration.get(key);
         if (value == null) {
             l.debug("Nothing found in the hashmap, let's try to SMP");
             key = query(value);
-        } 
+        }
         if (value == null) {
             l.debug("Value is still null, let's run TSLSynchronizer");
-            TSLSynchronizer.sync();
+            //TSLSynchronizer.sync();
         }
-        
+
         // TODO
         return null;
     }
 
     /**
-     * What query will do. Check firstly if the value is one which is related to SMP (e.g., it 
-     * is a value available in the ServiceMetadata). If not, return null. If yes, then do 
-     * pack the record and do a DNS query to discover which SMP (e.g., use the SML). 
-     * If the SML returns no record, return null. Else, obtain the service metadata, 
+     * What query will do. Check firstly if the value is one which is related to SMP (e.g., it
+     * is a value available in the ServiceMetadata). If not, return null. If yes, then do
+     * pack the record and do a DNS query to discover which SMP (e.g., use the SML).
+     * If the SML returns no record, return null. Else, obtain the service metadata,
      * verify the signature (the Trust must be established by means of the eIDAS). <br/>
-     * If the trust is ok, obtain all the values, store all of them in the database and in the hashmap, 
-     * and return the one requested.  
+     * If the trust is ok, obtain all the values, store all of them in the database and in the hashmap,
+     * and return the one requested.
      * @param key The key to be searched in the SMP
      * @return the value if known, null in all the other cases
      */
     private String query(String key) {
-        
+
         l.debug("Checking if the key is searchable");
 
         /*
          * Which kind of keys we can search upon? We can search only
          * the keys that split by "." has three values, and the last is .WSE, the first the
-         * country code. 
+         * country code.
          */
-        
-        // The key is searchable if the isSmp bit is valued. 
+
+        // The key is searchable if the isSmp bit is valued.
 //        https://openncp.atlassian.net/wiki/display/ncp/20160602+-+Meeting+minutes%2C+Thursday%2C+June+2nd%2C+2016+-+OpenNCP+Technical+Committee+Meeting
-        
+
 //        String[] explodedKey = key.split("\\.");
-//        
+//
 //
 //        if (explodedKey.length == 3) {
 //            l.trace("The length of the key to be searched is good. ");
@@ -184,15 +185,15 @@ public final class ConfigurationManagerSMP {
 //            // SMP. Let's now run the SML, according with
 //            // https://joinup.ec.europa.eu/svn/cipaedelivery/tags/2.2.4/cipa-core/Implementation/java/cipa-sml-client-library/src/test/java/eu/europa/ec/cipa/sml/client/DNSRegistrationTest.java
 ////            run sml here
-//            
+//
 //        }
         l.debug("The key is not seaerchable or it can't be found");
         return null;
-        
+
     }
 
-   
-    
+
+
     /**
      * Get the endpoint URL for a specified country and a service name
      *
