4cd3ab09917b9e853ba14ea77dd3a34dd08564ba
SUBIGER Renaud <renaud.subiger@ext.ec.europa.eu>
2016-11-18 12:09:22 +0100

    Improve web app skeleton

--- openncp-gateway/src/main/java/eu/europa/ec/sante/ehdsi/openncp/web/WebApplicationInitializer.java
+++ openncp-gateway/src/main/java/eu/europa/ec/sante/ehdsi/openncp/web/WebApplicationInitializer.java
@@ -1,13 +1,14 @@
 package eu.europa.ec.sante.ehdsi.openncp.web;
 
 import eu.europa.ec.sante.ehdsi.openncp.cfg.WebMvcConfig;
+import eu.europa.ec.sante.ehdsi.openncp.cfg.WebSecurityConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+public class WebApplicationInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
     protected Class<?>[] getRootConfigClasses() {
-        return new Class<?>[0];
+        return new Class<?>[]{WebSecurityConfig.class};
     }
 
     @Override
